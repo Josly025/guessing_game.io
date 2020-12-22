5b72a19e24ccdf977341dde8ee3262d9bab31c41
Grantjos12 <grantjoslyn@gmail.com>
2020-12-21 20:42:25 -0600

    alert working for inadequate values

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
+const alert = document.getElementById("alert");
+let randomNumber = Math.floor(Math.random() * (20 - 1 + 1));
+
+let min = 1;
+let max = 20;
+
+submit.addEventListener("click", checkGuess);
+
+function checkGuess(e) {
+  e.preventDefault();
+  //make string into a value
+  const guess = parseInt(input.value);
+  if (isNaN(guess) | (guess > max) | (guess < min)) {
+    alert.innerHTML = ` <div class="alert alert-dismissible alert-danger">
+      <button type="button" class="close" data-dismiss="alert">
+        &times;
+      </button>
+      <strong>Try again!</strong>
+      <a href="#" class="alert-link">
+        Please guess a number between ${min} and ${max}.
+      </a>
+    </div>`;
+    setTimeout(function () {
+      alert.remove();
+    }, 4000);
+  } else if (guess != randomNumber) {
+    alert("incorrect answer");
+  }
+  console.log(guess);
+}
