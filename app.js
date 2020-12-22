30f9aee1ee11bece69fbe84c46be805c8db8d13d
Grantjos12 <grantjoslyn@gmail.com>
2020-12-21 20:58:22 -0600

    alert logic and rendering html working

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const input = document.getElementById("input");
 const submit = document.getElementById("submit");
 const alert = document.getElementById("alert");
-let randomNumber = Math.floor(Math.random() * (20 - 1 + 1));
+const warning = document.getElementById("warning");
+let correctAns = Math.floor(Math.random() * (10 - 1 + 1));
 
 let min = 1;
-let max = 20;
+let max = 10;
 
 submit.addEventListener("click", checkGuess);
 
 function checkGuess(e) {
   e.preventDefault();
   //make string into a value
-  const guess = parseInt(input.value);
+  let guess = parseInt(input.value);
   if (isNaN(guess) | (guess > max) | (guess < min)) {
     alert.innerHTML = ` <div class="alert alert-dismissible alert-danger">
       <button type="button" class="close" data-dismiss="alert">
@@ -25,8 +26,24 @@ function checkGuess(e) {
     setTimeout(function () {
       alert.remove();
     }, 4000);
-  } else if (guess != randomNumber) {
-    alert("incorrect answer");
+  } else if (guess === correctAns) {
+    warning.innerHTML = `<div class="alert alert-dismissible alert-success">
+      <button type="button" class="close" data-dismiss="alert">
+        &times;
+      </button>
+      <strong>Well done!</strong> You successfully guessed 
+      <a href="#" class="alert-link">
+        ${correctAns}
+      </a>
+      
+    </div>`;
+  } else {
+    warning.innerHTML = `<div class="alert alert-dismissible alert-warning">
+  <button type="button" class="close" data-dismiss="alert"></button>
+  <h4 class="alert-heading">Incorrect Number!</h4>
+  <p class="mb-0">Please Guess again.</a></p>
+    </div>`;
   }
+  console.log("this is correct answer " + correctAns);
   console.log(guess);
 }
